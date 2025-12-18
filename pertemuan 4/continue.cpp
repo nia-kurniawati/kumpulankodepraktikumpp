058da5fc95d44513a0fc10031b1b1fd626f65345
Nia Kurniawati <znmblinks@gmail.com>
2025-12-18 22:40:02 +0700

    Update continue.cpp

--- pertemuan 4/continue.cpp
+++ pertemuan 4/continue.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            continue; 
+            cout << i << " ";
+        }
+    }
+    cout << "\nLooping Selesai";
+    
+    return 0;
+}
