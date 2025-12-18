0ec88c73795ea6455d91bed0d00d22d4aa115c63
Nia Kurniawati <znmblinks@gmail.com>
2025-12-18 22:35:23 +0700

    Update praktikum.cpp

--- pertemuan 2/praktikum.cpp
+++ pertemuan 2/praktikum.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+string nama = "Nia Kurniawati";
+string NPM = "2517051016";
+
+cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+cout << "Nama saya adalah " << nama << endl;
+cout << "NPM saya adalah " << NPM << endl << endl;
+cout << "Unila Indah" << endl;
+cout << "Be Strong" << endl;
+}
